423d5a356277597f11446e543414e62447f2e382
navyasrinyalapatla <navyasrinyalapatla@gmail.com>
2024-08-14 12:30:57 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,55 @@
+let allMusic = [
+    {
+        name: "Anvesh",
+        artist: "SitaRamam",
+        img: "sita",
+        src: "sita"
+  
+    },
+ {
+        name: "Jalsa",
+        artist: "Pawan-Kalyan",
+        img: "jalsa",
+        src: "jalsa"
+    },
+
+ {
+        name: "Sita-Ramam",
+        artist: "Mrunal",
+        img: "sita_sad",
+        src: "sita_sad"
+    },
+
+ {
+        name: "Dharshana",
+        artist: "megha-akash",
+        img: "darshana",
+        src: "darshana"
+    },
+
+    {
+        name: "nee_yaro_yaro",
+        artist: "Nainathara",
+        img: "rajarani",
+        src: "rajarani"
+    },
+    {
+        name: "nee_valane_nenunna",
+        artist: "Prabhas",
+        img: "radheshyam",
+        src: "radheshyam"
+    },
+	 {
+        name: "minnu",
+        artist: "Prabhas",
+        img: "kalki",
+        src: "kalki"
+    },
+
+
+];
+    
+      
+ 
+
+
